6e1698d7a3b4ab8601f6a2647162d5fc2b497410
Sacha Vandermoeten <sachadu835@gmail.com>
2025-03-11 12:22:41 +0100

    feat: todo input construit en dynamique + ajout de la todo avec son id, value et condition en console

--- index.js
+++ index.js
@@ -19,16 +19,71 @@ displayBodyBackground(
   "./images/prairie.jpg"
 );
 
-function addNewTodos() {
-  // rechecker car todoContainer pas use et .all-todo-container non plus
-  const inputAdd = document.querySelector(".todo-manager");
+const todosData = {
+  todos: [],
+};
+
+function buildTodo() {
+  const todoManager = document.createElement("div");
+  todoManager.classList.add("todo-manager");
+
+  const theTodo = document.createElement("div");
+  theTodo.classList.add("the-todo");
+
+  const labelCheck = document.createElement("label");
+  labelCheck.classList.add("checkbox-container");
+  const inputCheck = document.createElement("input");
+  inputCheck.type = "checkbox";
+  inputCheck.id = "test";
+  const spanCheck = document.createElement("span");
+  spanCheck.classList.add("custom-checkbox");
+
+  const inputAdder = document.createElement("input");
+  inputAdder.type = "text";
+  inputAdder.id = "input-adder";
+
+  const btnDeleteTodo = document.createElement("i");
+  btnDeleteTodo.classList.add("fa-solid", "fa-trash-can");
+  btnDeleteTodo.id = "btn-delete-todo";
+
+  labelCheck.appendChild(inputCheck);
+  labelCheck.appendChild(spanCheck);
+  theTodo.appendChild(labelCheck);
+  theTodo.appendChild(inputAdder);
+  todoManager.appendChild(theTodo);
+  todoManager.appendChild(btnDeleteTodo);
+
+  return todoManager;
+}
+
+function addNewTodo() {
+  const inputAdd = document.getElementById("input-adder");
+  if (inputAdd) {
+    inputAdd.addEventListener("change", () => {
+      inputAddValue = {
+        id: new Date().getTime(),
+        text: inputAdd.value,
+        completed: false,
+      };
+      todosData.todos.push(inputAddValue);
+      console.log(todosData.todos);
+    });
+  } else {
+    console.error(
+      "WARNING: #input-adder is not available, check function saveTodo"
+    );
+  }
+}
+
+function displayTodoField() {
   const btnAdd = document.getElementById("btn-add-todo");
-  const todoContainer = document.querySelector(".card-main");
-  const indicator = document.querySelector(".indicator");
+  const todoContainer = document.querySelector(".all-todos-container");
 
   btnAdd.addEventListener("click", () => {
-    indicator.innerText = "";
-    const unput = (inputAdd.style.display = "flex");
+    todoContainer.innerText = "";
+    todoContainer.appendChild(buildTodo());
+    addNewTodo();
   });
 }
-addNewTodos();
+
+displayTodoField();
